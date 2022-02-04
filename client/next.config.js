70193567489b1a220f5ff6298690a360742433fb
Leena WH <wolee111@coupang.com>
2022-02-04 23:15:29 +0900

    edit packages

--- client/next.config.js
+++ client/next.config.js
@@ -4,4 +4,12 @@ module.exports = {
   sassOptions: {
     includePaths: [path.join(__dirname, './pages')],
   },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
 };
