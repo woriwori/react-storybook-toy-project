3e012cebba42d6ec8a2fadae8edebea408da87f8
Leena WH <wolee111@coupang.com>
2022-02-04 16:01:22 +0900

    initial config

---
+++ client/src/pages/_app.js
@@ -0,0 +1,15 @@
+import './index.scss';
+
+/*
+ * next 의 필수 파일
+ * 아래 코드는 기본 템플릿
+ * */
+
+const App = ({ Component, pageProps }) => <Component {...pageProps} />;
+
+App.getInitialProps = async ({ ctx, Component }) => {
+  const pageProps = await Component.getInitialProps?.(ctx);
+  return { pageProps };
+};
+
+export default App;
---
+++ client/src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import UserList from '@/UserList';
+
+const Home = () => {
+  return (
+    <div>
+      <UserList />
+    </div>
+  );
+};
+
+export default Home;
