f2b6241095731d14249837a32b7abfd7148b820c
Leena WH <wolee111@coupang.com>
2022-02-04 17:18:09 +0900

    edit page design

--- client/src/pages/users.js
+++ client/src/pages/users.js
@@ -1,19 +1,34 @@
 import React from 'react';
+import styled from 'styled-components';
 import { Button } from 'antd';
-import { UserOutlined } from '@ant-design/icons';
+import { UserOutlined, PlusOutlined } from '@ant-design/icons';
 import CommonTitle from '@/CommonTitle';
 
-const TITLE_TEXT = 'Users';
+const StyledUsers = styled.div`
+  width: 1000px;
+  #table-test {
+    height: 500px;
+    background-color: cornflowerblue;
+  }
+`;
+const StyledHeader = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
 const Users = () => {
+  const TITLE_TEXT = 'Users';
   return (
-    <div>
-      <CommonTitle
-        text={TITLE_TEXT}
-        IconComponent={UserOutlined}
-        IconComponentProps={{ className: 'test' }}
-      />
-      <Button type="primary">Primary Button</Button>
-    </div>
+    <StyledUsers>
+      <StyledHeader>
+        <CommonTitle text={TITLE_TEXT} IconComponent={UserOutlined} />
+        <Button type="primary" shape="round" icon={<PlusOutlined />}>
+          Add
+        </Button>
+      </StyledHeader>
+      <div id="table-test"></div>
+    </StyledUsers>
   );
 };
 
