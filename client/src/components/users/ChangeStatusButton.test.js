9cbf2930987937bf3c674ca4dc7366fb14f57868
Leena WH <wolee111@coupang.com>
2022-03-09 01:28:28 +0900

    edit ChangeStatusButton test code

--- client/src/components/users/ChangeStatusButton.test.js
+++ client/src/components/users/ChangeStatusButton.test.js
@@ -1,12 +1,46 @@
 import React from 'react';
+import userEvent from '@testing-library/user-event';
 import { render, screen } from '@testing-library/react';
 import { composeStories } from '@storybook/testing-react';
+import '@testing-library/jest-dom'; // for using toBeVisible
 import * as stories from './ChangeStatusButton.stories';
 
 const { Default } = composeStories(stories);
 
-test('renders primary button with default arsd', () => {
+beforeAll(() => {
   render(<Default />);
-  const buttonElement = screen.getByText('Deactivate');
-  expect(buttonElement).not.toBeNull();
+});
+describe('', () => {
+  test('renders button', () => {
+    const buttonElement = screen.getByText('Deactivate');
+    expect(buttonElement).not.toBeNull();
+  });
+
+  test('modal should open when a button is clicked', () => {
+    const buttonElement = screen.getByText('Deactivate');
+    buttonElement.click();
+
+    const title = screen.getByText('Change Status');
+    const content = screen.getByText('Deactivate this user?');
+    const ok = screen.getByText('OK');
+    const cancel = screen.getByText('Cancel');
+
+    expect(title).not.toBeNull();
+    expect(content).not.toBeNull();
+    expect(ok).not.toBeNull();
+    expect(cancel).not.toBeNull();
+  });
+
+  test('modal should close when the Cancel button is clicked', async () => {
+    const buttonElement = screen.getByText('Deactivate');
+    buttonElement.click();
+
+    const modalTitle = screen.getByText('Change Status');
+    expect(modalTitle).toBeVisible();
+
+    const modalCancel = screen.getByText('Cancel');
+    userEvent.click(modalCancel);
+
+    expect(modalTitle).not.toBeVisible();
+  });
 });
